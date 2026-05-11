11b9bfedff6d41213fe1cf639a57d0dfcc96dfd4
SOLO <solo@local>
2026-05-11 17:16:46 +0800

    deploy: knowgrph build sync 2026-05-11T09:1646Z

---
+++ knowgrph/assets/schemaWorkspaceFiles-SEBJA4P7.js
@@ -0,0 +1,7 @@
+import{u as c,n,am as r,W as s}from"./index-CMi0Gmf7.js";import{f as o}from"./Canvas-Pf65lCSb.js";const i="/schema",a="/schema/schema.json";async function h(){const e=await r();await e.ensureSeed();try{await e.createFolder({parentPath:s,name:"schema"})}catch{}}async function m(){await h();const e=await r();try{await e.readFileText(a);return}catch{}const t=(()=>{try{return JSON.stringify(c.getState().schema,null,2)}catch{return`{
+  "catalog": {
+    "nodeTypes": [],
+    "edgeLabels": []
+  }
+}
+`}})();try{await e.createFile({parentPath:i,name:"schema.json",text:t})}catch{try{await e.writeFileText(a,t)}catch{}}}function l(){try{c.getState().setWorkspaceViewMode("editor")}catch{}(async()=>{try{await m(),o.getState().setActivePath(n(a))}catch{}})()}export{a as S,l as o};
--- knowgrph/assets/workspaceEditorModePresentation-BBmZ6IkM.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./Canvas-Bipwplh8.js";const t={table:e.tableViewLabel,multiDimTable:e.titleDefault,kanban:e.kanbanViewLabel};function b(a){return t[a]}export{b as g};
