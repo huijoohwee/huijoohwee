8f530f44fac144c5d6c2b3bbc691a6b079267fb6
SOLO <solo@local>
2026-05-22 23:15:51 +0800

    Publish local 3d camera WebMCP inspector

--- content/knowgrph/assets/schemaWorkspaceFiles-C7yJLdeJ.js
+++
@@ -1,7 +0,0 @@
-import{bn as r,u as n,bo as s,bh as o,bi as c,bp as i}from"./index-DUYb9_rK.js";const h="/schema",e="/schema/schema.json";async function p(){const a=await c();await a.ensureSeed();try{await a.createFolder({parentPath:i,name:"schema"})}catch{}}async function S(){await p();const a=await c();try{await a.readFileText(e);return}catch{}const t=(()=>{try{return JSON.stringify(n.getState().schema,null,2)}catch{return`{
-  "catalog": {
-    "nodeTypes": [],
-    "edgeLabels": []
-  }
-}
-`}})();try{await a.createFile({parentPath:h,name:"schema.json",text:t})}catch{try{await a.writeFileText(e,t)}catch{}}}function l(){r(n.getState()),(async()=>{try{await S(),s.getState().setActivePath(o(e))}catch{}})()}export{e as S,l as o};
--- knowgrph/assets/mainPanelSettingsSelectClass-C80VqNgU.js
+++
@@ -1,1 +0,0 @@
-import{U as t}from"./index-DUYb9_rK.js";const _=["App-toolbar__btn","text-xs","min-w-0","max-w-full",t.button.text,t.button.hoverBg].join(" ");export{_ as M};
