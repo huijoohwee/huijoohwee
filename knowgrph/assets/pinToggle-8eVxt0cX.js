0088ca94e10185dce02a22c9a40dc9115137c6d0
SOLO <solo@local>
2026-05-30 16:14:10 +0800

    Publish knowgrph

--- knowgrph/assets/pinToggle-8eVxt0cX.js
+++
@@ -1,1 +0,0 @@
-import{gU as n,dW as t}from"./index-D1ZaY0yt.js";function r(o){return o?n("App-toolbar__btn",t.button.activeBg,t.icon.active):"App-toolbar__btn"}export{r as g};
---
+++ knowgrph/assets/schemaWorkspaceFiles-Cl4lJ33y.js
@@ -0,0 +1,7 @@
+import{mc as r,qn as n,qr as s,m9 as o,jy as c,eb as i}from"./index-BMd90TnP.js";const h="/schema",a="/schema/schema.json";async function m(){const e=await c();await e.ensureSeed();try{await e.createFolder({parentPath:i,name:"schema"})}catch{}}async function S(){await m();const e=await c();try{await e.readFileText(a);return}catch{}const t=(()=>{try{return JSON.stringify(n.getState().schema,null,2)}catch{return`{
+  "catalog": {
+    "nodeTypes": [],
+    "edgeLabels": []
+  }
+}
+`}})();try{await e.createFile({parentPath:h,name:"schema.json",text:t})}catch{try{await e.writeFileText(a,t)}catch{}}}function l(){r(n.getState()),(async()=>{try{await S(),s.getState().setActivePath(o(a))}catch{}})()}export{a as S,l as o};
