92fec9c3ffc55631b974d1e385db05e99d062214
SOLO <solo@local>
2026-06-25 09:46:50 +0800

    chore: publish knowgrph prod assets

---
+++ content/knowgrph/assets/workspaceEditorModePresentation-Bt-tzOgD.js
@@ -0,0 +1,1 @@
+import{bc as e}from"./settings-mcp-docs-6Z1VuOL3.js";const t={table:e.tableViewLabel,multiDimTable:e.titleDefault,kanban:e.kanbanViewLabel};function l(a){return t[a]}export{l as g};
---
+++ knowgrph/assets/richMediaPanelDefaults-D-h3ZoDP.js
@@ -0,0 +1,1 @@
+import{W as E}from"./index-_c2mUhOq.js";const _=E.width,I=Math.round(_*9/16),A=Object.freeze({width:_,height:I});export{_ as R,A as a,I as b};
